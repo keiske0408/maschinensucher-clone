d1e8fd791ba401b45e15e20370bbedb4d9fda62e
Je Rome <jeromeabelida19@gmail.com>
2025-08-18 00:18:22 +0800

    Update Footer.vue
    Added section 4 for footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
   <footer>
     <section class="footer-section-container">
       <div class="container-for-sections">
-        <section class="footer-1">
+        <section class="footer-sponsoring">
           <div class="sponsor-title">
             <div>
               <p class="official-sponsors-title">
@@ -13,23 +13,193 @@
             </div>
           </div>
           <div class="sponsors-container">
-            <div class="sponsor-1">
+            <div class="sponsor">
               <a
-                class="sponsor-1-name"
+                class="sponsor-name"
                 href="https://company.maschinensucher.de/sponsoring/"
               >
                 <img
-                  class="EHF_finals_image_logo"
+                  class="EHF_finals"
                   src="https://cdn.machineseeker.com/img/frontend/template/footer/sponsoring/ehf_men.svg?v=1742898169"
-                  alt="EHF_finals_image_logo"
+                  alt="EHF-finals-image-logo"
+                />
+              </a>
+            </div>
+            <div class="sponsor">
+              <a
+                class="sponsor-name"
+                href="https://company.maschinensucher.de/sponsoring/"
+              >
+                <img
+                  class="snooker"
+                  src="https://cdn.machineseeker.com/img/frontend/template/footer/sponsoring/machineseeker_snooker.svg?v=1739437402"
+                  alt="snooker-logo"
+                />
+              </a>
+            </div>
+            <div class="sponsor">
+              <a
+                class="sponsor-name"
+                href="https://company.maschinensucher.de/sponsoring/"
+              >
+                <img
+                  class="Darts-EM"
+                  src="https://cdn.machineseeker.com/img/frontend/template/footer/sponsoring/machineseeker_european_championship.svg?v=1739437402"
+                  alt="Darts-EM-logo"
+                />
+              </a>
+            </div>
+            <div class="sponsor">
+              <a
+                class="sponsor-name"
+                href="https://company.maschinensucher.de/sponsoring/"
+              >
+                <img
+                  class="EHF-Champions-League"
+                  src="https://cdn.machineseeker.com/img/frontend/template/footer/sponsoring/ehf_b_ms_de.svg?v=1739437402"
+                  alt="EHF-Champions-League-logo"
+                />
+              </a>
+            </div>
+            <div class="sponsor">
+              <a
+                class="sponsor-name"
+                href="https://company.maschinensucher.de/sponsoring/"
+              >
+                <img
+                  class="Morlock-Motors"
+                  src="https://cdn.machineseeker.com/img/frontend/template/footer/sponsoring/morlock_motors.svg?v=1739437402"
+                  alt="Morlock-Motors-logo"
                 />
               </a>
             </div>
           </div>
         </section>
-        <section id="2"></section>
-        <section id="3"></section>
-        <section id="4"></section>
+        <section class="footer-telephone-number">
+          <div class="telephone-number-container">
+            <div class="telephone-number-flags-container">
+              <div class="contact-container">
+                <div class="contact">
+                  <div class="flag-container">
+                    <img
+                      src="https://cdn.machineseeker.com/img/frontend/main/flags/blank24.png?v=1739437402"
+                      alt="flag-1"
+                      class="flag"
+                    />
+                  </div>
+                  <div class="contact-number">
+                    <a href="">+492018578610</a>
+                  </div>
+                </div>
+              </div>
+              <div class="contact-container">
+                <div class="contact">
+                  <div class="flag-container">
+                    <img
+                      src="https://cdn.machineseeker.com/img/frontend/main/flags/blank24.png?v=1739437402"
+                      alt="flag-1"
+                      class="flag"
+                    />
+                  </div>
+                  <div class="contact-number">
+                    <a href="">+492018578610</a>
+                  </div>
+                </div>
+              </div>
+              <div class="contact-container">
+                <div class="contact">
+                  <div class="flag-container">
+                    <img
+                      src="https://cdn.machineseeker.com/img/frontend/main/flags/blank24.png?v=1739437402"
+                      alt="flag-1"
+                      class="flag"
+                    />
+                  </div>
+                  <div class="contact-number">
+                    <a href="">+492018578610</a>
+                  </div>
+                </div>
+              </div>
+              <div class="contact-container">
+                <div class="contact">
+                  <div class="flag-container">
+                    <img
+                      src="https://cdn.machineseeker.com/img/frontend/main/flags/blank24.png?v=1739437402"
+                      alt="flag-1"
+                      class="flag"
+                    />
+                  </div>
+                  <div class="contact-number">
+                    <a href="">+492018578610</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="footer-seo-listing-bar">
+          <div class="footer-seo-listing-title-container">
+            <div class="footer-seo-listing-title">
+              <p>Aktuelle Maschinensuchen:</p>
+            </div>
+          </div>
+          <div class="seo-names-container">
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Fördertechnik & Lagertechnik</a
+              >
+            </div>
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Kettenstemmer Ersatzteile</a
+              >
+            </div>
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Fp4</a
+              >
+            </div>
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Paletten-Freilagerregale</a
+              >
+            </div>
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Säulenschwenkkran</a
+              >
+            </div>
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Senk Errodiermaschine</a
+              >
+            </div>
+            <div class="seo-name">
+              <a href="www.maschinensucher.de/mss/senk+errodiermaschine"
+                >Säge</a
+              >
+            </div>
+          </div>
+        </section>
+        <section class="footer-links-sections">
+          <div class="footer-links-container">
+            <div class="footer-links-rows">
+              <div class="footer-verkaufen-container">
+                <span class="verkaufen">Verkaufen</span>
+                <div class="footer-inserate-container">
+                  <a href="https://www.maschinensucher.de/tariffs/">
+                    <span>
+                      Inserate schalten
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </section>
         <section id="5"></section>
         <section id="6"></section>
       </div>
@@ -44,7 +214,7 @@ footer {
   width: 1860px;
 }
 
-.footer-1 {
+.footer-sponsoring {
   background-color: #515150;
   height: 10rem;
   padding: 24px 12px;
@@ -75,17 +245,319 @@ footer {
   align-content: center;
 }
 
-.sponsor-1 {
+.sponsor {
   display: flex;
   align-items: center;
+  border-right: 1px solid #787979;
 }
 
-.sponsor-1-name {
+.sponsor:last-child {
+  border-right: none;
+}
+
+.sponsor-name {
   padding: 0px 16px;
 }
 
-.EHF_finals_image_logo {
+.EHF_finals {
   width: 82px;
   height: 58px;
+}
+
+.snooker {
+  width: 188px;
+  height: 58px;
+}
+
+.Darts-EM {
+  width: 188px;
+  height: 58px;
+}
+
+.Darts-EM {
+  width: 138px;
+  height: 58px;
+}
+
+.EHF-Champions-League {
+  width: 220px;
+  height: 58px;
+}
+
+.Morlock-Motors {
+  width: 82px;
+  height: 58px;
+}
+
+.footer-telephone-number {
+  height: 112px;
+  background-color: #3c3c3c;
+  padding: 0px 12px;
+}
+
+.telephone-number-container {
+  height: 112px;
+  width: 116px;
+  padding: 0px 12px;
+  margin: 0px 348px;
+}
+
+.telephone-number-flags-container {
+  width: 1140px;
+  height: 112px;
+  padding: 24px 0px 24px 16px;
+  display: flex;
+  font-family: "Open Sans ", sans-serif;
+  box-sizing: border-box;
+  font-weight: 400;
+  flex-wrap: wrap;
+  overflow-wrap: break-word;
+}
+
+.contact-container {
+  width: 374px;
+  height: 32px;
+  padding: 4px 12px;
+
+  text-align: start;
+  overflow-wrap: break-word;
+  font-family: "Open Sans", sans-serif;
+  color: white;
+}
+
+.contact {
+  display: flex;
+  width: 374px;
+  height: 24px;
+}
+
+.flag-container {
+  width: 24px;
+  height: 24px;
+}
+
+.flag {
+  width: 24px;
+  height: 16px;
+}
+
+.contact-number {
+  height: 24px;
+  width: 351px;
+  box-sizing: border-box;
+  font-weight: 400;
+}
+
+.contact-number a {
+  width: 134px;
+  height: 17px;
+}
+
+.footer-seo-listing-bar {
+  width: 1860px;
+  height: 92px;
+  padding: 12px 12px;
+  background-color: #3c3c3c;
+}
+
+.footer-seo-listing-title-container {
+  width: 1860px;
+  height: 40px;
+}
+
+.footer-seo-listing-title {
+  padding: 0px 12px;
+  width: 1860px;
+  height: 40px;
+}
+
+.footer-seo-listing-title p {
+  margin-bottom: 16px;
+  text-align: center;
+  color: white;
+  width: 1836px;
+  height: 24px;
+}
+
+.seo-names-container {
+  width: 1836px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 16px;
+  color: rgb(170, 170, 170);
+  
+}
+
+.seo-name:nth-child(1) {
+  width: 216px;
+  height: 24px;
+}
+
+.seo-name:nth-child(1) a {
+  width: 216px;
+  height: 19px;
+  padding: 0px 16px;
+  font-size: 14px;
+  border-right-width: 1px;
+  border-right-color: rgb(80, 80, 80);
+}
+
+.seo-name:nth-child(2) {
+  width: 197px;
+  height: 24px;
+  overflow-wrap: break-word;
+}
+
+.seo-name:nth-child(2) a {
+  width: 197px;
+  height: 24px;
+  padding: 0px 16px;
+  font-size: 14px;
+  border-right-width: 1px;
+  border-right-color: rgb(80, 80, 80);
+}
+
+.seo-name:nth-child(3) {
+  width: 57.13px;
+  height: 24px;
+  overflow-wrap: break-word;
+}
+
+.seo-name:nth-child(3) a {
+  width: 57.13px;
+  height: 24px;
+  padding: 0px 16px;
+  font-size: 14px;
+  border-right-width: 1px;
+  border-right-color: rgb(80, 80, 80);
+}
+
+.seo-name:nth-child(4) {
+  width: 183px;
+  height: 24px;
+  overflow-wrap: break-word;
+}
+
+.seo-name:nth-child(4) a {
+  width: 183px;
+  height: 24px;
+  padding: 0px 16px;
+  font-size: 14px;
+  border-right-width: 1px;
+  border-right-color: rgb(80, 80, 80);
+}
+
+.seo-name:nth-child(5) {
+  width: 159px;
+  height: 24px;
+  overflow-wrap: break-word;
+}
+
+.seo-name:nth-child(5) a {
+  width: 159px;
+  height: 24px;
+  padding: 0px 16px;
+  font-size: 14px;
+  border-right-width: 1px;
+  border-right-color: rgb(80, 80, 80);
+}
+
+.seo-name:nth-child(6) {
+  width: 179px;
+  height: 24px;
+  overflow-wrap: break-word;
+}
+
+.seo-name:nth-child(6) a {
+  width: 179px;
+  height: 24px;
+  padding: 0px 16px;
+  font-size: 14px;
+  border-right-width: 1px;
+  border-right-color: rgb(80, 80, 80);
+}
+
+.seo-name:nth-child(7) {
+  width: 68px;
+  height: 24px;
+  overflow-wrap: break-word;
+}
+
+.seo-name:nth-child(7) a {
+  width: 68px;
+  height: 24px;
+  padding: 0px 16px;
+  font-size: 14px;
+}
+
+.footer-links-sections{
+  width: 1860px;
+  height: 272px;
+  padding: 0px 12px;
+  background-color:#2C2D2C;
+}
+
+.footer-links-container{
+  width: 1140px;
+  height: 272px;
+  margin: 0px 348px;
+  padding: 0px 12px;
+}
+
+.footer-links-rows{
+  padding: 16px 0px;
+  margin: 0px -12px;
+  width: 1140;
+  height: 272px;
+  display: flex;
+  flex-wrap: flex;
+}
+
+.footer-verkaufen-container{
+  width: 138.406px;
+  height: 239.188px;
+  color: rgb(45, 45, 45);
+  padding: 16px 12px;
+
+}
+
+.verkaufen{
+  width: 115px;
+  height: 20px;
+  color: rgb(250, 250, 250);
+  font-weight: 400;
+  margin-bottom: 8px;
+  line-height: 19.2px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+}
+
+.footer-inserate-container{
+  width: 115px;
+  height: 21px;
+  background-color:rgb(45, 45, 45);
+  margin-bottom: 24px;
+  text-align: start;
+}
+
+.footer-inserate-container a{
+  width: 107px;
+  height: 17px;
+  text-align: center;
+  box-sizing: border-box;
+  overflow-wrap: break-word;
+}
+
+.footer-inserate-container a span{
+  width: 107px;
+  height: 17px;
+  color: rgb(190, 190, 190);
+  font-size: 14px;
+  font-family: "Open Sans", sans-serif;
+}
+
+.footer-inserate-container a span:hover{
+  text-decoration: underline;
 }
 </style>
